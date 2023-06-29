dfeea69a7f12efaf86936e2832ab29f2a436cc63
omoev <109823323+omoev@users.noreply.github.com>
2023-06-29 21:00:18 +0600

    Add files via upload

---
+++ DZ4_26_02/app.js
@@ -0,0 +1,18 @@
+const  btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () => {
+  const request = new XMLHttpRequest()
+  request.open("GET", "data.json")
+  request.setRequestHeader("Content-Type", "application/json")
+  request.send()
+
+  request.addEventListener("load", () => {
+    const data = JSON.parse(request.response);
+    console.log(data)
+
+    for (const person in data[0]) {
+      const parents = data[0][person].parents
+      console.log(parents)
+    }
+  })
+})
